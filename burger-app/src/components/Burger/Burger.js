10b7d34efc2368ab24fa8e6ead7ad07d249ff45e
gitfilix <runlevel07@gmail.com>
2018-09-08 21:32:15 +0200

    dynamic state of ingredients

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -5,11 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    console.log("burger props:", props);
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                console.log("props.ingredients", props.ingredients);
+                console.log("igKey", igKey);
+                return <BurgerIngredient key={igKey + 1} type={igKey} />
+            });
+        });
+    console.log("transformedIngredients", transformedIngredients);
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
