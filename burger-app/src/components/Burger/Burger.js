0fc95e7d2d509e529f9025284f056baf17d76e23
gitfilix <runlevel07@gmail.com>
2018-09-16 21:05:31 +0200

    reduced array and checked failback if no ingredients are specified

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -5,18 +5,28 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    console.log("burger props:", props);
-    const transformedIngredients = Object.keys(props.ingredients)
+    // console.log("burger props:", props);
+    // Object.keys methods transformes(returns) object keys (NOT Values) into an array
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
+            // create an Array with spread operator for props ingredients
             return [...Array(props.ingredients[igKey])].map((_, i) => {
-                console.log("props.ingredients", props.ingredients);
-                console.log("igKey", igKey);
+                // console.log("props.ingredients", props.ingredients);
+                //console.log("igKey", igKey);
                 return <BurgerIngredient key={igKey + 1} type={igKey} />
             });
-        });
+        })
+        // flatten array by reduce
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>please add some ingredients!</p>
+        }
     console.log("transformedIngredients", transformedIngredients);
 
     return(
+
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
             {transformedIngredients}
